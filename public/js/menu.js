463f724982427b30790e8eb9b9af66812b8f47b1
Joel Wiklund <joel.wiklund@me.com>
2018-11-19 16:31:25 +0100

    JSON

---
+++ public/js/menu.js
@@ -0,0 +1,28 @@
+var MenuItems = [
+  {
+  "name": "Meat burger",
+  "stock": 1,
+  "kCal": 100,
+  "lactose": true,
+  "gluten": true,
+  "img": "https://www.chatelaine.com/wp-content/uploads/2017/05/Bibimbap-homemade-burgers.jpg"
+  }
+  ,
+  {
+  "name": "Fish burger",
+  "stock": 1,
+  "kCal": 200,
+  "lactose": false,
+  "gluten": true,
+  "img": "https://www.flcbalbriggan.ie/wp-content/uploads/2017/01/FLC-Fish-Burger.jpg"
+  }
+  ,
+  {
+  "name": "Vegan burger",
+  "stock": 1,
+  "kCal": 300,
+  "lactose": false,
+  "gluten": false,
+  "img": "https://joyfoodsunshine.com/wp-content/uploads/2017/04/Baked-Sweet-Potato-Black-Bean-Veggie-Burgers-gluten-free-4.jpg"
+  }
+]
